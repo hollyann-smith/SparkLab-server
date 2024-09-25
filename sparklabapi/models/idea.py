ce7202cc630a038329f0aa6646c01b28ea18175e
Holly Smith <hollyschuldt@gmail.com>
2024-09-25 11:54:32 -0500

    push

--- sparklabapi/models/idea.py
+++ sparklabapi/models/idea.py
@@ -5,8 +5,8 @@ from sparklabapi.models.user import User
 
 class Idea(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    title = models.CharField(max_length=800)
+    description = models.CharField(max_length=800)
     saved = models.BooleanField()
-    img = models.URLField(max_length=200)
+    img = models.URLField(max_length=800)
     supplies = models.ManyToManyField(Supply, related_name='ideas')
--- sparklabapi/models/user.py
+++ sparklabapi/models/user.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField(max_length=200)
+    username = models.CharField(max_length=500)
     uid = models.CharField(max_length=50)
